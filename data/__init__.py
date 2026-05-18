24c4a751a8daa352a389ceb694c5e67baeb98ddc
Benjamin Jiang <162191305+benjiangyx@users.noreply.github.com>
2026-05-18 15:55:20 -0700

    Add data quality tracking and refactor splits API

--- data/__init__.py
+++ data/__init__.py
@@ -1,12 +1,7 @@
 """Data pipeline for nuScenes keyframes with action labels."""
 
 from data.dataset import NuScenesFrameDataset
-from data.splits import (
-    count_samples_per_split,
-    generate_mini_splits,
-    get_split_from_canonical,
-    verify_no_overlap,
-)
+from data.splits import create_action_splits
 from data.embeddings import load_all_embeddings, load_embeddings, load_encoder_embedding
 from data.temporal import TemporalEmbeddingDataset
 from data.transforms import load_and_preprocess_image, validate_tensor_range
@@ -15,6 +10,7 @@ from data.z_hat import load_z_hat, load_z_real
 __all__ = [
     "NuScenesFrameDataset",
     "TemporalEmbeddingDataset",
+    "create_action_splits",
     "load_and_preprocess_image",
     "load_all_embeddings",
     "load_embeddings",
@@ -22,8 +18,4 @@ __all__ = [
     "load_z_hat",
     "load_z_real",
     "validate_tensor_range",
-    "get_split_from_canonical",
-    "generate_mini_splits",
-    "verify_no_overlap",
-    "count_samples_per_split",
 ]
